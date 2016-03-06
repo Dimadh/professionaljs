d707124b485f06ad4d12578059a4c7ac39342405
sychd <denis.sych@gmail.com>
2016-03-06 17:18:10 +0200

    sort and visualised

--- hw0/script.js
+++ hw0/script.js
@@ -2,6 +2,7 @@ var arr = new Array;
 
 var innerI = 0,//for pseudoloop
 	outerI = 0;
+
 function start() {
 	arr = setArray();
 	var arrBox = document.getElementById('arr-box');
@@ -34,42 +35,63 @@ function eraseArrBox(arr,arrBox){
  	for (var i = 0; i < arr.length; i++) {
  		document.getElementById('elem'+i).remove();
  	}
- }
+}
 
 function addElem(index,val,parent) {
- 	var elemWidth = 50;//.arr-elem width css property
+	var elemWidth = 50;//.arr-elem width css property
 
- 	var elem = document.createElement('div');
- 	elem.id = 'elem'+index;
- 	elem.className = 'arr-elem';
- 	elem.style.left= index*elemWidth +'px';
- 	elem.innerHTML = val;
- 	parent.appendChild(elem);
- }
+	var elem = document.createElement('div');
+	elem.id = 'elem'+index;
+	elem.className = 'arr-elem';
+	elem.style.left= index*elemWidth +'px';
+	elem.innerHTML = val;
+	parent.appendChild(elem);
+}
 
 
 
- function nextClicked(){
- 	if(outerI<arr.length){
- 		//alert('outer: ' +outerI + ' inner: '+ innerI + ' arr: ' + arr);
- 		if(innerI<arr.length-outerI){
- 			if (arr[innerI]>arr[innerI+1]){
- 				//alert(arr[innerI] +'>' + arr[innerI+1]);
- 				var temp = arr[innerI];
- 				arr[innerI] = arr[innerI+1];
- 				arr[innerI+1] = temp;
- 			}
- 			innerI++;
- 			if(innerI == arr.length-outerI){
+function nextClicked(){
+	var elem1,elem2;
+
+	if(outerI<arr.length){
+		if(innerI<arr.length-outerI){
+			undyeElems();
+			elem1 = document.getElementById('elem'+innerI);
+			elem2 = document.getElementById('elem'+(1+innerI));
+			
+			dyeElem(elem1,elem2,'#FFFF3C');
+
+			if (arr[innerI]>arr[innerI+1]){
+				var temp = arr[innerI];
+				arr[innerI] = arr[innerI+1];
+				arr[innerI+1] = temp;
+				dyeElem(elem1,elem2,'#FF533C');
+				changeElem(elem1,elem2,arr[innerI],arr[innerI+1]);
+			}
+
+			innerI++;
+			if(innerI == arr.length-outerI){
 				innerI =0;
 				outerI++;
+				undyeElems();
 			}
- 		}
- 	}else{	
- 		alert(arr);
- 	}
- }
+		}
+	}
+}
+function undyeElems(){
+	for (var i = 0; i < arr.length; i++) {
+		document.getElementById('elem'+i).style.backgroundColor='#F9F9F9';
+	}
+}
+function dyeElem(elem1,elem2,color){
+	elem1.style.backgroundColor = color;
+	if(elem2)
+		elem2.style.backgroundColor = color;
+}
 
- function clearBox(){
- 	alert(arr);
- }
+function changeElem(elem1,elem2,val1,val2){
+	elem1.innerHTML = val1;
+	elem2.innerHTML = val2; 
+}
+function clearBox(){
+}
